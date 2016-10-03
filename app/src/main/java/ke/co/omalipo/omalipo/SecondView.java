17f4fbc6dc77de8bf1ffd4a13dc4382c01a06a2c
Petermuturi <pmuturi65@gmail.com>
2016-10-03 10:02:46 +0300

    added TabLayout

---
+++ app/src/main/java/ke/co/omalipo/omalipo/SecondView.java
@@ -0,0 +1,29 @@
+package ke.co.omalipo.omalipo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondView extends Fragment {
+
+
+    public SecondView() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second_view, container, false);
+    }
+
+}
